c399653941b62871a78d3fecf54ad4da61900122
ammarsultan <ammar.sultan@edu.umi.ac.ma>
2024-03-01 15:08:41 +0100

    task screen design settled up.

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -52,6 +52,7 @@ class TasksScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: EdgeInsets.all(20.0),
               height: 300.0,
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -59,6 +60,31 @@ class TasksScreen extends StatelessWidget {
                   topRight: Radius.circular(20.0),
                   topLeft: Radius.circular(20.0),
                 ),
+              ),
+              child: ListView(
+                children: [
+                  ListTile(
+                    title: Text('This is a task'),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('This is a task'),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                  ListTile(
+                    title: Text('This is a task'),
+                    trailing: Checkbox(
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
